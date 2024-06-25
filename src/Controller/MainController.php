642f7dfe30596f840447ea72dab527690852c307
LorisJa <loris.jaquerod2024@campus-eni.fr>
2024-06-25 17:01:57 +0200

    home / afficher sorties (broken)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main_home')]
-    public function home(): Response
+    public function home(SortieRepository $sortieRepository): Response
     {
-        return $this->render('main/home.html.twig');
+        $sorties = $sortieRepository->findAll();
+
+        return $this->render('main/home.html.twig', [
+            'sorties' => $sorties,
+        ]);
     }
 }
