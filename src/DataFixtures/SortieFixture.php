7b94bdd7fdf352ee7cb04459a3462d12b3044ab6
LorisJa <loris.jaquerod2024@campus-eni.fr>
2024-06-26 15:33:58 +0200

    edit ficture

--- src/DataFixtures/SortieFixture.php
+++ src/DataFixtures/SortieFixture.php
@@ -36,9 +36,12 @@ class SortieFixture extends Fixture implements FixtureGroupInterface, OrderedFix
         for ($i = 0; $i < 10; $i++) {
             $sortie = new Sortie();
             $sortie->setNom($faker->sentence(3));
-            $sortie->setDateHeureDebut($faker->dateTimeBetween('-10 days', '+25 days'));
-            $sortie->setDuree($faker->numberBetween(60, 360));
-            $sortie->setDateLimiteInscription($faker->dateTimeBetween('-10 days', '+25 days'));
+            $dateLimiteInscription = $faker->dateTimeBetween('-15 days', '+15 days');
+            $sortie->setDateLimiteInscription($dateLimiteInscription);
+            $dateHeureDebut = clone $dateLimiteInscription;
+            $dateHeureDebut->modify('+' . $faker->numberBetween(3, 15) . ' days');
+            $sortie->setDateHeureDebut($dateHeureDebut);
+            $sortie->setDuree($faker->numberBetween(60, 180));
             $sortie->setNbInscriptionMax($faker->numberBetween(5, 20));
             $sortie->setInfoSortie($faker->text(200));
             $sortie->setCampus($faker->randomElement($tabCampus));
