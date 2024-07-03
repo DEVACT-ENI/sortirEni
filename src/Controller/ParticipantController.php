c3903aba184212dbf8bf47cd730f11b303f746bc
mlemeur2023 <lemeur.mathis2023@campus-eni.fr>
2024-07-03 16:37:33 +0200

    je sais pas quoi dire à par CSS / HTML c'est lonnnnnnnnnnnnnnnnnnnnnnng

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\PhotoProfilType;
 use App\Form\ModifProfilType;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
@@ -21,28 +22,38 @@ class ParticipantController extends AbstractController
     #[Route('/modif-profil', name: 'modif_profil', methods: ['GET', 'POST'])]
     public function modifProfil(ParticipantRepository $participantRepository, Request $request, UserPasswordHasherInterface $hasher, FileUploader $fileUploader): Response
     {
-        $participant = $this->getUser();
-        $form = $this->createForm(ModifProfilType::class, $participant);
+        $participant =  $participantRepository->find($this->getUser()->getId());
+        $participant2 = clone $participant;
+        $form = $this->createForm(ModifProfilType::class, $participant2);
+        $formPhoto = $this->createForm(PhotoProfilType::class);
+        $formPhoto->handleRequest($request);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($form->get('password')->getData() != null)
-                $participant->setPassword($hasher->hashPassword(
-                    $participant,
-                    $form->get('password')->getData()));
-
+        if ($formPhoto->isSubmitted() && $formPhoto->isValid()) {
             /** @var UploadedFile $file */
-            $file = $form['photo']->getData();
+            $file = $formPhoto['photo']->getData();
 
             if ($file)
                 $fileName = $fileUploader->upload($file, (string)$participant->getId());
+            return $this->redirectToRoute('participants_modif_profil');
+        }
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump("j'ai passe la validation");
+            if ($form->get('password')->getData() != null)
+                $participant2->setPassword($hasher->hashPassword(
+                    $participant,
+                    $form->get('password')->getData()));
+
+            $participant->copyFrom($participant2);
             $participantRepository->save($participant);
-            return $this->redirectToRoute('main_home');
+            $this->addFlash('success', 'Profil modifié avec succès');
+            return $this->redirectToRoute('participants_modif_profil');
         }
 
         return $this->render('participant/modif-profil.html.twig', [
             'form' => $form->createView(),
+            'formPhoto' => $formPhoto->createView(),
             'participant' => $participant,
         ]);
     }
--- src/Form/ModifProfilType.php
+++ src/Form/ModifProfilType.php
@@ -6,7 +6,6 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
